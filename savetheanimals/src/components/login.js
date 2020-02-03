ddcc1e8665eaaeeb57fb951d00dd4e27886282f9
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-03 15:45:04 -0600

    login logic done waiting on backend for the url

--- savetheanimals/src/components/login.js
+++ savetheanimals/src/components/login.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 
 const Login = props => {
     const [user, setUser] = useState({
@@ -6,16 +7,42 @@ const Login = props => {
         password: ''
     });
 
+    const {history} = props;
+
+    const handleChange = event =>
+    {
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const login = event =>
+    {
+        event.preventDefault();
+        axios.post('', user)
+        .then(response =>
+        {
+            console.log(response);
+            // localStorage.setItem('token', );
+            // history.push('/');
+        })
+        .catch(error => 
+        {
+            console.log(error);
+        })
+    }
+
     return (
-        <form>
+        <form onSubmit={login}>
             <div>
                 <label htmlFor='name'>Username </label>
-                <input id='name' type='text' name='name' placeholder='Full Name'/>
+                <input id='name' type='text' name='username' placeholder='Full Name' onChange={handleChange} value={user.username}/>
             </div>
             
             <div>
                 <label htmlFor='password'>Password </label> 
-                <input id='password' type='password' name='name' placeholder='Full Name'/>
+                <input id='password' type='password' name='password' placeholder='Full Name' onChange={handleChange} value={user.password}/>
             </div>
 
             <button type='login'>Login</button>
