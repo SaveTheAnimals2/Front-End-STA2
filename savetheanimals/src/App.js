4b8dd2cc9224644b5a7ee7064a75c062261e068e
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-03 15:01:40 -0600

    added in Route to App.js

--- savetheanimals/src/App.js
+++ savetheanimals/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter as Router, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import PrivateRoute from './componets/PrivateRoute';
 import './App.css';
 
