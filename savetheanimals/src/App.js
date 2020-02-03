af1ba2f39378bdb2606a6e0a474175969a769304
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-03 13:51:51 -0600

    added in router

--- savetheanimals/src/App.js
+++ savetheanimals/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 
 function App(props) {
   const {isLoading, error} = props;
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      
+    </Router>
   );
 }
 
