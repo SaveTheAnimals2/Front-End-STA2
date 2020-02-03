dc9bf39a86958207c7368227e4066882065c1ba3
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-03 14:25:03 -0600

    added in logic to handle success and failure and added campaigns to state

--- savetheanimals/src/reducers/index.js
+++ savetheanimals/src/reducers/index.js
@@ -1,12 +1,33 @@
 const initalState = {
     isLoading: false,
-    error: ''
+    error: '',
+    campaigns: []
 };
 
 export const reducer = (state = initalState, action) =>
 {
     switch(action.type)
     {
+        case 'GET_CAMPAIGNS':
+            return {
+                ...state,
+                isLoading: true
+            }
+
+            case 'SUCCESS':
+                return {
+                    ...state,
+                    campaigns: action.payload,
+                    isLoading: false
+                }
+
+            case 'FAILURE':
+                return {
+                    ...state,
+                    error: action.payload,
+                    isLoading: false
+                }
+
         default:
             return state;
     }
