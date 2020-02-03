5102b57b935581039dd415a0d0a0376103c60161
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-03 14:01:24 -0600

    added a route for the login component and added a private route for the dashboard after login

--- savetheanimals/src/App.js
+++ savetheanimals/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
+import PrivateRoute from './componets/PrivateRoute';
 import './App.css';
 
 function App(props) {
   const {isLoading, error} = props;
   return (
     <Router>
-      
+      <Switch>
+        {/* <Route exact path = '/' component={Login}/> */}
+        {/* <PrivateRoute path = '' component={}/> */}
+      </Switch>
     </Router>
   );
 }
