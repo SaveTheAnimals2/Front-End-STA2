aca3c921a994361a961a7a675bfdaf8eaa2792e1
alexis villaraza <alexis.borja@gmail.com>
2020-02-03 12:35:27 -0700

    added the submit a new campaign form

---
+++ savetheanimals/src/components/CampaignForm.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+const Campaign = props => {
+    const [campaign, setCampaign] = useState({
+        name: '',
+        location: '',
+        species: '',
+        description: '',
+        urgency: '',
+        goal: '',
+        deadline: '',
+
+    });
+
+    const handleChanges = e => {
+        setMember({...campaign, [e.target.name]: e.target.value});
+    };
+
+    return (
+        <form>
+            <div>
+                <label htmlFor='name'>Campaign Name </label>
+                <input id='name' type='text' name='name' placeholder='Campaign Name' onChange={handleChanges}/>
+            </div>
+
+            <div>
+                <label htmlFor='location'>Location </label>
+                <input id='location' type='text' name='location' placeholder='city/country' onChange={handleChanges}/>
+            </div>
+            
+            <div>
+                <label htmlFor='species'>Species </label> 
+                <input id='species' type='text' name='species' placeholder='type of animal' onChange={handleChanges}/>
+            </div>
+
+            <div>
+                <label htmlFor='description'>Description of what your campaign will do </label> 
+                <textarea id='desc' type='text' name='desc' onChange={handleChanges}/>
+            </div>
+
+            <div>
+                <label htmlFor='select'>Urgency Level </label>
+                <select id='urgency' type='select' name='location' placeholder='select urgency' onChange={handleChanges}>
+                    <option/>
+                    <option>Low</option>
+                    <option>Medium</option>
+                    <option>High</option>
+                    <option>Critical</option>
+                </select>
+            </div>
+
+            <button type='submit'>I'm an Organization</button>
+            <button type='submit'>I'm a Supporter</button>
+
+            <div>
+                <label htmlFor='goal'>Goal Amount </label> 
+                <input id='goal' type='text' name='goal' placeholder='$' onChange={handleChanges}/>
+            </div>
+
+            <div>
+                <label htmlFor='deadline'>Deadline </label> 
+                <input id='deadline' type='date' name='date' placeholder='MM/DD/YYYY' onChange={handleChanges}/>
+            </div>
+
+            <button type='submit'>Add Campaign</button>
+
+
+        </form>
+    )
+};
+
+export default Campaign;
