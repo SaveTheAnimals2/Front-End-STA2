29d660c5a66c4764d548706f3dfe76bb6ffe26eb
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-03 16:16:41 -0600

    added dashbaord component for privateroute after user logs in

---
+++ savetheanimals/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard()
+{
+    return(
+        <div className='user-dashboard'>
+            <h1>Welcome to your dashboard</h1>
+        </div>
+    );
+}
+
+export default Dashboard;
