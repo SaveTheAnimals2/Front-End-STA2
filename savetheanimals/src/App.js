9293fe5e2092900f59842a40c48098d1a15c9615
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-03 16:28:26 -0600

    removed redux stuff from app.js to dashboard.js makes more sense to add it there

--- savetheanimals/src/App.js
+++ savetheanimals/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Login'
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
-import {getCampaigns, addCampaign, updateCampaign, deleteCampaign} from './actions';
 import './App.css';
 
-function App(props) {
-  const {isLoading, error, campaigns, isUpdating, getCampaigns, addCampaign, updateCampaign, deleteCampaign} = props;
+function App() {
   return (
     <Router>
       <Switch>
@@ -22,14 +19,4 @@ function App(props) {
   );
 }
 
-const mapStateToProps = state =>
-{
-  return {
-    isLoading: state.isLoading,
-    error: state.error,
-    campaigns: state.campaigns,
-    isUpdating: state.isUpdating
-  }
-}
-
-export default connect(mapStateToProps, {getCampaigns, addCampaign, updateCampaign, deleteCampaign})(App);
+export default App;
--- savetheanimals/src/components/Dashboard.js
+++ savetheanimals/src/components/Dashboard.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {getCampaigns, addCampaign, updateCampaign, deleteCampaign} from '../actions';
 
-function Dashboard()
+function Dashboard(props)
 {
+    const {isLoading, error, campaigns, isUpdating, getCampaigns, addCampaign, updateCampaign, deleteCampaign} = props;
     return(
         <div className='user-dashboard'>
             <h1>Welcome to your dashboard</h1>
@@ -9,4 +12,15 @@ function Dashboard()
     );
 }
 
-export default Dashboard;
+const mapStateToProps = state =>
+{
+  return {
+    isLoading: state.isLoading,
+    error: state.error,
+    campaigns: state.campaigns,
+    isUpdating: state.isUpdating
+  }
+}
+
+
+export default connect(mapStateToProps, {getCampaigns, addCampaign, updateCampaign, deleteCampaign})(Dashboard);
