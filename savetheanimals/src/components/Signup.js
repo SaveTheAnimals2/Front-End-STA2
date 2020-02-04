20bbf3105f1fb11d3852991e3ce8d965df787d18
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-04 12:23:16 -0600

    changed the flow of login page slightly to display an organization name when you click on i'm an organization

--- savetheanimals/src/components/Signup.js
+++ savetheanimals/src/components/Signup.js
@@ -3,10 +3,13 @@ import axios from 'axios';
 
 const Signup = props => {
     const [user, setUser] = useState({
-        name: '',
+        username: '',
         email: '',
-        password: ''
+        password: '',
+        organizationName: ''
     });
+
+    const [isOrganization, setIsOrganization] = useState(false);
 
     const {history} = props;
 
@@ -21,21 +24,27 @@ const Signup = props => {
     const register = event =>
     {
         event.preventDefault();
-        axios.post('', user)
-        .then(response =>
-        {
-            console.log(response);
-            // localStorage.setItem('token', );
-            // history.push('/');
-        })
+        console.log(user);
+        // axios.post('https://save-the-animal-buildweek.herokuapp.com/api/auth/register', user)
+        // .then(response =>
+        // {
+        //     // localStorage.setItem('token', );
+        //     // history.push('/');
+        // })
 
+    }
+
+    const handleClick = event =>
+    {
+        event.preventDefault();
+        setIsOrganization(true);
     }
 
     return (
         <form onSubmit={register}>
             <div>
-                <label htmlFor='name'>Name </label>
-                <input id='name' type='text' name='name' placeholder='Full Name' onChange={handleChanges} value={user.name}/>
+                <label htmlFor='name'>UserName </label>
+                <input id='name' type='text' name='username' placeholder='username' onChange={handleChanges} value={user.username}/>
             </div>
 
             <div>
@@ -48,7 +57,14 @@ const Signup = props => {
                 <input id='password' type='password' name='password' placeholder='password' onChange={handleChanges} value={user.password}/>
             </div>
 
-            <button type='submit'>I'm an Organization</button>
+            <button onClick={handleClick}>I'm an Organization</button>
+            {isOrganization && (
+            <div>
+                <label htmlFor='organization'>Organization </label> 
+                <input id='organizaton' type='text' name='organizationName' placeholder='organization name' onChange={handleChanges} value={user.organizationName}/>
+                <button type='submit'>Submit</button>
+            </div>
+            )}
             <button type='submit'>I'm a Supporter</button>
 
         </form>
