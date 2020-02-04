0c307fa04e8a6644ab2da8b4e843fe449c932fe0
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-04 12:41:28 -0600

    added an onClick to the add campaign button to go to the CampaignForm

--- savetheanimals/src/components/Dashboard.js
+++ savetheanimals/src/components/Dashboard.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {getCampaigns, addCampaign, updateCampaign, deleteCampaign} from '../actions';
+import { useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getCampaigns, addCampaign, updateCampaign, deleteCampaign } from '../actions';
 import Campaign from './Campaign';
 
-function Dashboard(props)
+function Dashboard(props) 
 {
-  const {isLoading, error, campaigns, isUpdating, getCampaigns, addCampaign, updateCampaign, deleteCampaign, history} = props;
-    return(
-        <div className='user-dashboard'>
-            <h1>Welcome to your dashboard</h1>
-            <div className='campaign-container'>
-              {campaigns === undefined || campaigns.length === 0 ? 
-              <p>Let's add a campign to see it here</p> : 
-              !isLoading && !isUpdating &&(
-                <Campaign campaigns={campaigns}/>
-              )}
-            </div>
-            <button onClick={() => history.push('/add-campaign')}>Add a Campaign</button>
-        </div>
-    );
+  const { isLoading, error, campaigns, isUpdating, getCampaigns, addCampaign, updateCampaign, deleteCampaign } = props;
+
+  const history = useHistory();
+
+  const handleClick = () =>
+  {
+    history.push('/add-campaign')
+  }
+
+  return (
+    <div className='user-dashboard'>
+      <h1>Welcome to your dashboard</h1>
+      <div className='campaign-container'>
+        {campaigns === undefined || campaigns.length === 0 ?
+          <p>Let's add a campign to see it here</p> :
+          !isLoading && !isUpdating && (
+            <Campaign campaigns={campaigns} />
+          )}
+      </div>
+      <button onClick={() => history.push('/add-campaign')}>Add a Campaign</button>
+    </div>
+  );
 }
 
-const mapStateToProps = state =>
-{
+const mapStateToProps = state => {
   return {
     isLoading: state.isLoading,
     error: state.error,
@@ -32,4 +40,4 @@ const mapStateToProps = state =>
 }
 
 
-export default connect(mapStateToProps, {getCampaigns, addCampaign, updateCampaign, deleteCampaign})(Dashboard);
+export default connect(mapStateToProps, { getCampaigns, addCampaign, updateCampaign, deleteCampaign })(Dashboard);
