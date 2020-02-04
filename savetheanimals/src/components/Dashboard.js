a04fcdb9ff1c64ecbfb0c1f5ab986f563c15f72f
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-04 11:14:37 -0600

    added logic to render campaign component if something is in the campaign state otherwise render a message saying there's nothing in that state

--- savetheanimals/src/components/Dashboard.js
+++ savetheanimals/src/components/Dashboard.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {getCampaigns, addCampaign, updateCampaign, deleteCampaign} from '../actions';
+import Campaign from './Campaign';
 
 function Dashboard(props)
 {
-    const {isLoading, error, campaigns, isUpdating, getCampaigns, addCampaign, updateCampaign, deleteCampaign} = props;
+  const {isLoading, error, campaigns, isUpdating, getCampaigns, addCampaign, updateCampaign, deleteCampaign} = props;
     return(
         <div className='user-dashboard'>
             <h1>Welcome to your dashboard</h1>
+            <div className='campaign-container'>
+              {campaigns === undefined || campaigns.length === 0 ? 
+              <p>Let's add a campign to see it here</p> : 
+              !isLoading &&(
+                <Campaign campaigns={campaigns}/>
+              )}
+            </div>
         </div>
     );
 }
