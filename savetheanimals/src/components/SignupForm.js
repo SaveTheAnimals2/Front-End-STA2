081d2252ce28ef7ce1b523f8f3516f1c70bc2ace
alexis villaraza <alexis.borja@gmail.com>
2020-02-04 19:37:30 -0700

    added validation to password on sign up form

--- savetheanimals/src/components/SignupForm.js
+++ savetheanimals/src/components/SignupForm.js
@@ -1,68 +1,104 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 import { useForm } from "react-hook-form";
 
-import './SignUp.css';
+import "./SignUp.css";
 
 const SignupForm = props => {
 
-console.log("!", props)
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-    const [user, setUser] = useState({
-        name: '',
-        email: '',
-        password: '',
+  const [passwordError, setPasswordError] = useState({
+      passwordErrorStatus: ""
+  })
+
+  const { history } = props;
+
+  const handleChanges = event => {
+
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value
     });
+  };
 
-    const {history} = props;
+  const register = event => {
+    event.preventDefault();
 
-    const handleChanges = event => {
-        setUser({
-            ...user,
-            [event.target.name]: event.target.value
-        })
+    if (user.password.length <= 8) {
+        setPasswordError(true)
+    } else {
+        setPasswordError(false);
     }
 
-    const register = event => {
+          setUser({name: "", email: "", password: ""})
 
-        event.preventDefault();
-        
-        axios.post('', user).then(response => {
-            console.log(response);
-            // localStorage.setItem('token', );
-            // history.push('/');
-        });        
-    }
+    axios.post("", user).then(response => {
+      console.log(response);
+      // localStorage.setItem('token', );
+      // history.push('/');
+        console.log(user);
+    });
+  };
 
-    return (
-        <div className='signup-form'>
-            <div className='signup-formbox'>
+  return (
+    <div className="signup-form">
+      <div className="signup-formbox">
         <form onSubmit={register}>
-            <h1>Sign Up</h1>
-            <div>
-                <label htmlFor='name'>Name</label>
-                <input id='name' type='text' name='name' placeholder='Full Name' onChange={handleChanges} value={user.name} />
-            </div>
+          <h1>Sign Up</h1>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              placeholder="Full Name"
+              onChange={handleChanges}
+              value={user.name}
+            />
+          </div>
 
-            <div>
-                <label htmlFor='email'>Email</label>
-                <input id='email' type='email' name='email' placeholder='email@email.com' onChange={handleChanges} value={user.email} />
-            </div>
-            
-            <div>
-                <label htmlFor='password'>Password</label> 
-                <input id='password' type='password' name='password' placeholder='password' onChange={handleChanges} value={user.password} />
-            </div>
-            
-            <div>
-                <button type='submit'>I'm an Organization</button>
-            </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="email@email.com"
+              onChange={handleChanges}
+              value={user.email}
+            />
+          </div>
+          
 
-            <div>
-                <button type='submit'>I'm a Supporter</button>
-            </div>
-        </form></div></div>
-    )    
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="password"
+              onChange={handleChanges}
+              value={user.password}
+            />
+            { passwordError === true ? 'Password too short' : ''}
+          </div>
+
+          <div>
+            <button type="submit">I'm an Organization</button>
+          </div>
+
+          <div>
+            <button type="submit">I'm a Supporter</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignupForm;
