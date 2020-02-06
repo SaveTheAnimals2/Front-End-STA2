822b90a4b24bfacea2327a11e46a2a3b4b33494a
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-06 13:16:26 -0600

    deploy to netlify

---
+++ savetheanimals/build/precache-manifest.2445de9ed0d959d97b36eeb141efd5de.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b49cbcae4e948b06558037d66d2268",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c04d69fa57b6ffbb708",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c71d0e84caffcc96c781",
+    "url": "/static/css/main.eea4a6b6.chunk.css"
+  },
+  {
+    "revision": "8c04d69fa57b6ffbb708",
+    "url": "/static/js/2.be3dd5fc.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.be3dd5fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71d0e84caffcc96c781",
+    "url": "/static/js/main.4349647f.chunk.js"
+  },
+  {
+    "revision": "ab3a3bd10592affb7007",
+    "url": "/static/js/runtime-main.6f089b2c.js"
+  },
+  {
+    "revision": "a5d1370ab4e48e3d93c022b7d41a3044",
+    "url": "/static/media/cat.a5d1370a.jpg"
+  },
+  {
+    "revision": "5e7f04f6d04546219d6ba7c8c30b754d",
+    "url": "/static/media/dolphins.5e7f04f6.jpg"
+  },
+  {
+    "revision": "ae45b2ec327adca6469770391fefb343",
+    "url": "/static/media/zebras.ae45b2ec.jpg"
+  }
+]);
